d388d06fa7c54d93d8cbeb9180b8601866060121
lm <leo.mangutov@gmail.com>
2019-04-26 13:32:14 -0700

    add statis beer data

--- src/components/ListOfBeers.jsx
+++ src/components/ListOfBeers.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Beer from "./Beer";
 
-function BeerList(){
+function ListOfBeers(){
   return (
     <Beer/>
   );
 }
 
-export default BeerList;
+export default ListOfBeers;
