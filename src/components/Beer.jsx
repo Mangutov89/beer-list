3f0957cf0b475cc618e263c9cfde245af116173e
lm <leo.mangutov@gmail.com>
2019-04-26 14:19:08 -0700

    added to beer component and styled it

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-function Beer(){
+function Beer(props){
   return (
     <div>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase entries not saving!</em></p>
-        <hr/>
-      </div>
+      <style jsx> {`
+        h3 {
+          font-size: 50px;
+          font-weight: bold;
+          font-family: monospace;
+          border: 5px solid blue;
+          padding: 10px 20px;
+          margin-bottom: 5%;
+        }
+      `}
+      </style>
+      <h3>{props.name}</h3>
+      <h4>Brewed in: {props.brewery}</h4>
+      <p>Alcohol Percent: {props.alcoholPercent}</p>
+      <p>Brewed from this region: {props.region}</p>
+      <hr/>
+    </div>
   );
 }
+
+Beer.propTypes = {
+  name: PropTypes.string,
+  brewery: PropTypes.string,
+  alcoholPercent: PropTypes.string,
+  region: PropTypes.string
+};
 
 export default Beer;
