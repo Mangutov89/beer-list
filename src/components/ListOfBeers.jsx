ffde5301981c2cffe45fa62b2b8b05e1fca97675
lm <leo.mangutov@gmail.com>
2019-04-26 14:47:32 -0700

    add loop to my beer list

--- src/components/ListOfBeers.jsx
+++ src/components/ListOfBeers.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Beer from "./Beer";
+import availableBeers from '../modelData/BeerList'
 
 function ListOfBeers(){
   return (
-    <Beer/>
+    <div>
+      {availableBeers.map((beer, index) =>
+        <Beer name = {beer.name}
+          brewery = {beer.brewery}
+          alcoholPercent = {beer.alcoholPercent}
+          region = {beer.region}
+          key = {index}/>
+      )}
+    </div>
   );
 }
 
