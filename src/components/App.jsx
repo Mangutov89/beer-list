f4507f643efebc895179ce411992fe2ec90e4da8
lm <leo.mangutov@gmail.com>
2019-04-26 17:15:55 -0700

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import ListOfBeers from "./ListOfBeers";
 
+import { Switch, Route } from 'react-router-dom';
+
 function App(){
   return (
     <div>
       <Header/>
-      <ListOfBeers/>
-      <h1>hello world</h1>
+        <Switch>
+          <Route exact path='/' component={ListOfBeers} />
+        </Switch>
     </div>
   );
 }
